9a96bf6fb3508a24ad78b0109cd4b8afc94e920c
Dmitriy Ziakhor <dmziakhor@gmail.com>
2022-01-03 02:20:24 +0200

    Final version

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,24 @@ const select3 = document.getElementById('select3');
 const select4 = document.getElementById('select4');
 const select5 = document.getElementById('select5');
 const sort = document.getElementById('select6');
+const openers = document.querySelectorAll('.question__opener');
+const closers = document.querySelectorAll('.question__closer');
+const nav = document.getElementById('navOnSmallSreen');
+const toggler = document.getElementById('toggler');
+const closeNav = document.getElementById('closeNav');
+const checkbox = document.getElementById('checkbox');
+
+for (let i = 0; i < openers.length; i++) {
+  const questions = document.querySelectorAll('.question');
+
+  openers[i].addEventListener('click', () => {
+    questions[i].className = 'question question--active';
+  });
+
+  closers[i].addEventListener('click', () => {
+    questions[i].className = 'question';
+  });
+}
 
 function getSelect(id) {
   const select = document.getElementById(`${id}-block`);
@@ -54,3 +72,19 @@ select3.addEventListener('click', (event) => getSelect(event.target.id));
 select4.addEventListener('click', (event) => getSelect(event.target.id));
 select5.addEventListener('click', (event) => getSelect(event.target.id));
 sort.addEventListener('click', (event) => getSelect(event.target.id));
+toggler.addEventListener('click', () => {
+  nav.className = 'navigation__small-sreen navigation__small-sreen--active';
+});
+closeNav.addEventListener('click', () => {
+  nav.className = 'navigation__small-sreen';
+});
+
+checkbox.addEventListener('change', (event) => {
+  const check = document.getElementById('check');
+
+  if (event.target.checked) {
+    check.className = 'cars-selection__check cars-selection__check--checked';
+  } else {
+    check.className = 'cars-selection__check';
+  }
+});
